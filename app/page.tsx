3d026fb7941388712f013569b09cdf0b0cc5898b
Serg <>
2024-07-28 21:06:21 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import "./second-global.css";
+
+export default function Home() {
+  return (
+    <ul className="cards">
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Front-1024x1024.jpg" width="1200" height="1200" />
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--A-Thousand-Clouds-Front-Cover-1024x1024.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Odd-World-EP-SMALL-1024x1024.png" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Forest-Blue-Pre-LR-1024x1024.png" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--MOM-Remixes-1024x1024.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--ALONE-TOGETHER-remix-V71-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Nature-Therapy-Pre-LR-550x550.png" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Deep-Dive-Ambient-Edits-V8-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Alpine-Koresma-x-Aroth-Artwork-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Out-Of-the-Dark-Cover-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--2020-06-07-Marley-Carroll-Single-Blue-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Deep-Dive-EP-Cover-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Blackrose-Cover-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--FW-Imagine-Gold-Remixes2-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--We-Are-COVER-MAIN-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Bath-House-Flat-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Pronoia-JPG-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Lapa9Theory-Cracking-Stores-no-Loci-logo-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--EMANCIPATOR_MOM_AlbumCover-2000px-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--MURGE-ep-cvr-3000x3000-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--Emancipator_Laybrinth-ART-01-4000x4000-at-300dpi-550x550.jpg" width="1200" height="1200"/>
+        </li>
+        <li>
+            <Image draggable="false" src="https://assets.codepen.io/89905/coverflow--cover-550x550.jpg" width="1200" height="1200"/>
+        </li>
+    </ul>
+  );
+}
